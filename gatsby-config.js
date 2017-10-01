b42e7660eadb535ae242e3a23fd3d82eb637fc30
EricSSartorius <EricSSartorius@gmail.com>
2017-10-01 18:31:16 +0200

    added react16 and typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-next`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
     },
     'gatsby-transformer-remark'
   ],
