9607e5b96cc2e43b97cc3112e38052d99811cdf3
EricSSartorius <EricSSartorius@gmail.com>
2017-11-06 11:16:27 +0000

    font src updated

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,7 @@ import Typography from "typography"
 const typography = new Typography({
   baseFontSize: '18px',
   baseLineHeight: 1.45,
-  headerFontFamily: ['BigNoodleTitling Oblique', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  headerFontFamily: [ 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
   bodyFontFamily: ['Roboto', 'sans-serif'],
  })
 
